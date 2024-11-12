54a4e7365fcb870f73f132aa4b95566ce3128886
Mozhaev Andrey <andr-mozha@yandex.ru>
2024-11-12 17:36:28 +0300

    git-rebase 2

--- branching/rebase.sh
+++ branching/rebase.sh
@@ -3,7 +3,11 @@
 
 count=1
 for param in "$@"; do
+<<<<<<< HEAD:branching/rebase.sh
     echo "\$@ Parameter #$count = $param"
+=======
+    echo "Next parameter: $param"
+>>>>>>> 2e70d11 (git-rebase 2):rebase.sh
     count=$(( $count + 1 ))
 done
 
